a1a438ad4094f3dbd1ecde747fa097f9ba50cfa8
Yaroslav <166743450+yaroslavmuzalevskyi@users.noreply.github.com>
2026-03-02 17:21:57 +0100

    removed social media icons from footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
 import FooterColumn from "@/components/common/FooterColumn";
 import Logo from "@/components/ui/Logo";
-import facebook from "@/public/icons/facebook-logo.svg";
-import twitter from "@/public/icons/twitter-logo.svg";
-import wtf from "@/public/icons/wtf-logo.svg";
-import instagram from "@/public/icons/instagram-logo.svg";
 
 const footerColumns = [
   {
@@ -31,13 +26,6 @@ const footerColumns = [
   },
 ];
 
-const socials = [
-  { label: "Facebook", icon: facebook, href: "#" },
-  { label: "Twitter", icon: twitter, href: "#" },
-  { label: "Instagram", icon: instagram, href: "#" },
-  { label: "WTF", icon: wtf, href: "#" },
-];
-
 export default function Footer() {
   return (
     <footer className="w-full border-t border-pr_w/30 bg-transparent">
@@ -48,24 +36,6 @@ export default function Footer() {
             <div className="text-pr_w/80">
               <p>+352 662 345 456</p>
               <p>contact@evervale.com</p>
-            </div>
-            <div className="flex gap-6 justify-center lg:justify-start">
-              {socials.map((social) => (
-                <a
-                  key={social.label}
-                  href={social.href}
-                  className="flex h-6 w-6 items-center justify-center"
-                  aria-label={social.label}
-                >
-                  <Image
-                    src={social.icon}
-                    alt={social.label}
-                    width={18}
-                    height={18}
-                    className="h-[18px] w-[18px] filter brightness-0 invert"
-                  />
-                </a>
-              ))}
             </div>
           </div>
 
