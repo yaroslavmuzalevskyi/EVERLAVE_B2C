9da21c317ebdf3e4d2659482a04b0bf9c7139ae5
Yaroslav <73663808+memerchik@users.noreply.github.com>
2026-06-02 17:45:18 +0200

    remove @next/third-parties & new GA4 integration

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,8 +3,11 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/components/auth/AuthProvider";
 import ScrollReveal from "@/components/ui/ScrollReveal";
-import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import Script from "next/script";
+
+const COOKIEYES_SCRIPT_SRC =
+  "https://cdn-cookieyes.com/client_data/07b210e7ce3aa326a31636fed7000cfc/script.js";
+const GTM_ID = "GTM-TQCNFKVW";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,14 +40,56 @@ export default function RootLayout({
       <head>
         <Script
           id="cookieyes"
-          src="https://cdn-cookieyes.com/client_data/72e63957d3c30f044edeeefae53db61e/script.js"
+          src={COOKIEYES_SCRIPT_SRC}
           strategy="beforeInteractive"
+        />
+        <Script
+          id="google-consent-default"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('consent', 'default', {
+                ad_storage: 'denied',
+                ad_user_data: 'denied',
+                ad_personalization: 'denied',
+                analytics_storage: 'denied',
+                functionality_storage: 'denied',
+                personalization_storage: 'denied',
+                security_storage: 'granted',
+                wait_for_update: 500
+              });
+            `,
+          }}
+        />
+        <Script
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({
+                'gtm.start': new Date().getTime(), event:'gtm.js'
+              });var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
         />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <GoogleTagManager gtmId="GTM-T3SWJ2DB" />
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <AuthProvider>
           <ScrollReveal />
           {children}
